c87c2460897ba5e6d504df61b7a2f5336de2bc63
swolebrain <thecodingteacher@gmail.com>
2018-10-27 00:04:53 -0400

    feat: app icon, needs to have transparency removed

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expo, {Font, Constants, Asset} from 'expo';
+import {Platform, View} from 'react-native';
 import {NativeRouter, Router, Route, Switch} from 'react-router-native';
 import NonAuthStackNavigator from './src/routes/NonAuthStackNavigator';
 import MainTabScreen from './src/routes/MainTabScreen';
@@ -8,6 +9,7 @@ import MainTabScreen from './src/routes/MainTabScreen';
 // import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
 import createMemoryHistory from 'history/createMemoryHistory';
 import userRepository from "./src/stores/UserDataStore";
+import FullPage from "./src/common/FullPage";
 // import { Provider } from 'mobx-react';
 //
 
@@ -33,13 +35,20 @@ class App extends React.Component {
     }
     render() {
         if (!this.state.resourcesLoaded) return <Expo.AppLoading />;
+        console.log(Constants);
+        let paddingView = null;
+        if (Platform.OS === 'ios' && Constants.statusBarHeight !== 0)
+            paddingView = <View style={{height: Constants.statusBarHeight, alignSelf: 'stretch'}} />
 
         return (
             <NativeRouter>
-                <Switch>
-                    <Route path='/maintabscreen' component={MainTabScreen} />
-                    <Route path='/' component={NonAuthStackNavigator} />
-                </Switch>
+                <FullPage>
+                    {paddingView}
+                    <Switch>
+                        <Route path='/maintabscreen' component={MainTabScreen} />
+                        <Route path='/' component={NonAuthStackNavigator} />
+                    </Switch>
+                </FullPage>
             </NativeRouter>
         )
     }
